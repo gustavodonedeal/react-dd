d854fb3b460f585b42683b9e38386488214729ed
Gustavo Godoi <gustavo@donedeal.ie>
2017-07-18 12:41:15 +0100

    Including section by url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import axios from 'axios';
 import history from './history';
 import Search from './components/Search';
 import View from './components/View';
@@ -8,57 +7,14 @@ import Header from './components/Header';
 
 class App extends Component  {
 
-  constructor(){
-      super();
-      this.loadAds = this.loadAds.bind(this);
-      this.updateAds = this.updateAds.bind(this);
-
-      // start the state
-      this.state = {
-        ads: [],
-        filters: {}
-      };
-  }
-
-  componentDidMount() {
-    this.loadAds();
-  }
-
-
-  loadAds(filters = null) {
-    const cors = 'https://cors-anywhere.herokuapp.com/';
-    const params = Object.assign({},{
-      adType: 'forsale',
-      max: 30,
-      sections: 'cars',
-      sort: 'relevance desc',
-    },filters) ;
-
-    this.serverRequest = axios.post(`${cors}https://www.donedeal.ie/search/api/v4/find/`, params)
-      .then((response) => {
-        this.setState({
-          ads: response.data.ads
-        })
-      }).catch((error) => {
-        console.error(error);
-        return [];
-      });
-  }
-
-  updateAds(filter) {
-    this.setState({
-      ads: []
-    });
-    this.loadAds(filter);
-  }
-
   render(){
     return(
       <Router history={history}>
         <div>
           <Header />
-          <Route exact path='/' component={() => <Search ads={this.state.ads} updateAds={this.updateAds} />}/>
+          <Route exact path='/' component={Search}/>
           <Route path='/ad/:adId' component={View}/>
+          <Route path='/:section' component={Search}/>
         </div>
       </Router>
     )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../App.css';
 import Card from './Card';
 import Filters from './Filters';
 
 class Search extends Component {
 
+  constructor(){
+      super();
+      this.loadAds = this.loadAds.bind(this);
+      this.updateAds = this.updateAds.bind(this);
+
+      // start the state
+      this.state = {
+        ads: []
+      };
+  }
+
+  componentDidMount() {
+    this.loadAds();
+  }
+
+
+  loadAds(filters = null) {
+    const cors = 'https://cors-anywhere.herokuapp.com/';
+    const section = this.props.match.params.section || 'cars';
+    const params = Object.assign({},{
+      adType: 'forsale',
+      max: 30,
+      section: section,
+      sort: 'relevance desc',
+    },filters) ;
+
+    this.serverRequest = axios.post(`${cors}https://www.donedeal.ie/search/api/v4/find/`, params)
+      .then((response) => {
+        this.setState({
+          ads: response.data.ads
+        })
+      }).catch((error) => {
+        console.error(error);
+        return [];
+      });
+  }
+
+  updateAds(filter) {
+    this.setState({
+      ads: []
+    });
+    this.loadAds(filter);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Search Results</h1>
-        <Filters updateAds={this.props.updateAds} />
+        <p><Filters updateAds={this.updateAds} /></p>
         <ul>
-          {this.props.ads.map((ad) =>
+          {this.state.ads.map((ad) =>
             <Card ad={ad} key={ad.id} />
           )}
-
         </ul>
-
       </div>
     );
   }
