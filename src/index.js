84285d21b4f41241fc60c1ae17c6cf0200c479a7
Ben <ben@donedeal.ie>
2017-07-18 09:45:03 +0100

    messing around

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import Search from './components/Search';
-import AdView from './components/AdView';
+import View from './components/View';
 import Header from './components/Header';
 // import registerServiceWorker from './registerServiceWorker';
 
@@ -13,7 +13,7 @@ const Root = () => {
       <div>
         <Header />
         <Route exact path='/' component={Search}/>
-        <Route path='/ad/:adId' component={AdView}/>
+        <Route path='/ad/:adId' component={View}/>
       </div>
     </Router>
   )
