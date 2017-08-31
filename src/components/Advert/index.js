9e1b6b8c87bd12180a0d47507b158f27518fd0d1
John Brennan <john.brennan@distilled.ie>
2017-08-31 17:51:35 +0100

    Basic advert

--- src/components/Advert/index.js
+++ src/components/Advert/index.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchAdvert } from './reducer';
+import * as advert from './reducer';
 import styles from './Advert.css';
 
 class Advert extends Component {
   async componentDidMount() {
     const { match, fetchAdvert, advert } = this.props;
-    if (!advert) await fetchAdvert(match.params.adId);
+    await fetchAdvert(match.params.adId);
   }
 
   render() {
+    const { header, description } = this.props;
     return (
       <div className={styles.advertContainer}>
-        <h1>Big car</h1>
-        {JSON.stringify(this.props.advert)}
+        <h1>{header}</h1>
+        <p>{description}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  advert: state.activeAdvert.advert
+  loading: advert.isLoading(state.activeAdvert),
+  header: advert.getHeader(state.activeAdvert),
+  description: advert.getDescription(state.activeAdvert)
 });
 
-export { fetchAdvert };
-export default connect(mapStateToProps, { fetchAdvert })(Advert);
+export default connect(mapStateToProps, { fetchAdvert: advert.fetchAdvert })(
+  Advert
+);
